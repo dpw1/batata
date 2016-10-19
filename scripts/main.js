0d40d633d291ba809cf3b82d639e1e0cb8fa49df
adargains <andre.dargains@gmail.com>
2016-10-19 21:27:39 +0100

    search searchBox textbox main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
-$(document).ready(function () {
+var BATATA = {
+     init: function(){
+        this.variables();
+        this.events();
+        this.initModules();
+    },
+    variables: function() {
+        
+    },
+    events: function() {
+        
+    },
+    initModules: function() {
+        $("#header").load("/modules/header.html");
+        $("#search").load("/modules/search.html");
+        $("#footer").load("/modules/footer.html");
+    }
+}
 
-});
+$(document).ready(function () { BATATA.init(); });
